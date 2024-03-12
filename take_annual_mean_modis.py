0384b6aecbc9a52325bc5dda96b8816e075b830b
grleung <gabrielle.leung@colostate.edu>
2024-03-12 11:50:48 -0600

    improved commenting on annual mean script

--- take_annual_mean_modis.py
+++ take_annual_mean_modis.py
@@ -1,4 +1,5 @@
-#Take downloaded modis data from download_modis_[var].py and get monthly & annual averages
+# Take downloaded modis data from download_modis_[var].py 
+# and get monthly & annual averages
 
 import pandas as pd
 import numpy as np
@@ -7,9 +8,14 @@ from jug import TaskGenerator,barrier
 
 @TaskGenerator
 def take_averages(yr, mo, name):
+    # this function will take the average of some downloaded MODIS data for a given year "yr" and month "mo"
+    
+    #decide which files to include
     if name == 'annual':
+        # if taking annual mean, just use all the months of that year
         paths = [f"{savePath}/{str(m).zfill(2)}/{str(yr)}.pkl" for m in range(1,13)]
     else:
+        # if taking monthly mean, use all the existing files under that month
         paths = [f"{dataPath}{p}" for p in sorted(os.listdir(dataPath)) 
              if (f"{str(yr)}_{str(mo).zfill(2)}" in p)]
 
@@ -18,6 +24,7 @@ def take_averages(yr, mo, name):
     if len(paths)==0:
         return()
 
+    # loop over each month in year or each file in month
     i = 0
     for p in paths:
         df = pd.read_pickle(p)
@@ -49,6 +56,7 @@ def take_averages(yr, mo, name):
     for var in vars:
         alldf[var] = alldf[f'{var}_mean_count']/alldf[f'{var}_count']
 
+    # concatenate all the data we read in and save it
     alldf = alldf[np.concatenate([vars,[f"{var}_count" for var in vars]])]
     alldf.to_pickle(f"{savePath}/{name}/{str(yr)}.pkl")
 
@@ -60,16 +68,19 @@ for sat in ['aqua','terra']:
             dataPath = f"/moonbow/gleung/satlcc/MODIS_data_{n}/"
             savePath =f"/moonbow/gleung/satlcc/MODIS_{n}/"
 
+            # check if right directory exists
             if not os.path.isdir(savePath):
                 os.mkdir(savePath)
                 
+            # use right year range depending on if using terra or aqua data
             if 'terra' in n:
                 yrs = range(2001,2021)
             else:
                 yrs = range(2003,2021)
 
+            # use right variables depending on which MODIS product being used
             if 'cf' in n:
-                vars = ['cf','cod','cth']
+                vars = ['cf','cth']
             elif 'wv' in n:
                 vars = ['pwat']
             elif 'aod' in n:
@@ -83,13 +94,14 @@ for sat in ['aqua','terra']:
                 if not os.path.exists(f"{savePath}/{str(mo).zfill(2)}/{str(yr)}.pkl"):
                     take_averages(yr,mo,str(mo).zfill(2)) 
 
-            #once all monthly averages are done
+            # wait until all monthly averages are done
             barrier()
 
-            #calculate annual average
+            # check if right directory exists
             if not os.path.isdir(f"{savePath}/annual/"):
                 os.mkdir(f"{savePath}/annual/")
-                
+               
+            #calculate annual average 
             for yr in yrs:
                 if not os.path.exists(f"{savePath}/annual/{str(yr)}.pkl"):
                     take_averages(yr,1,'annual')
